f33058cd6bcd56e642c6cb5ae3ffb44320886d10
James Traver <jtraverjr@gmail.com>
2016-05-02 09:44:14 -0500

    challenge: write these tests

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -8,4 +8,12 @@ describe Movie do
   it "should return a hash" do
     Movie.new.to_hash.class.should == Hash
   end
+
+  it "should be Top Gun" do
+    @tg = Movie.new.to_hash
+    @tg["Title"].should == "Top Gun"
+  end
+
+  # verify the year == 1986
+  # verify the rating == "PG"
 end
